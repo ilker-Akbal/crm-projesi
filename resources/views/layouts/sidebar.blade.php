50810b2e76e2dbfa2beed576f048bb4d467dcad9
ilker-Akbal <135032557+ilker-Akbal@users.noreply.github.com>
2025-07-10 08:32:15 +0300

    sidebar created

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -92,55 +92,272 @@
     </li>
   </ul>
 </li>
-        <li class="nav-item">
-          <a href="{{ route('products.index') }}" class="nav-link">
-            <i class="nav-icon fas fa-box-open"></i>
-            <p>Products</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ route('accounts.index') }}" class="nav-link">
-            <i class="nav-icon fas fa-handshake"></i>
-            <p>Accounts</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ route('reports.index') }}" class="nav-link">
-            <i class="nav-icon fas fa-chart-line"></i>
-            <p>Reports</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ route('support.index') }}" class="nav-link">
-            <i class="nav-icon fas fa-headset"></i>
-            <p>Support</p>
-          </a>
-        </li>
-        <li class="nav-item">
+        <!-- Products with dropdown -->
+<li class="nav-item has-treeview">
   <a href="#" class="nav-link">
-    <i class="nav-icon fas fa-tasks"></i>
-    <p>Actions</p>
+    <i class="nav-icon fas fa-box-open"></i>
+    <p>
+      Products
+      <i class="right fas fa-angle-left"></i>
+    </p>
   </a>
-</li>
-<li class="nav-item">
-  <a href="" class="nav-link">
-    <i class="nav-icon fas fa-user"></i>
-    <p>Customers</p>
-  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Add Product</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>View Products</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Product Stocks</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Product Prices</p>
+      </a>
+    </li>
+  </ul>
 </li>
 
-        <li class="nav-item">
-          <a href="{{ route('users.index') }}" class="nav-link">
-            <i class="nav-icon fas fa-users"></i>
-            <p>Users</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ route('reminders.index') }}" class="nav-link">
-            <i class="nav-icon fas fa-bell"></i>
-            <p>Reminders</p>
-          </a>
-        </li>
+        <li class="nav-item has-treeview">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-handshake"></i>
+    <p>
+      Accounts
+      <i class="right fas fa-angle-left"></i>
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Add Account</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>View Accounts</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Add Movement</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>View Movements</p>
+      </a>
+    </li>
+  </ul>
+</li>
+
+       <li class="nav-item has-treeview">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-chart-line"></i>
+    <p>
+      Reports
+      <i class="right fas fa-angle-left"></i>
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Sales Report</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Customer Report</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Product Stock Report</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Current Account Summary</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Support Request Report</p>
+      </a>
+    </li>
+  </ul>
+</li>
+
+        <li class="nav-item has-treeview">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-headset"></i>
+    <p>
+      Support
+      <i class="right fas fa-angle-left"></i>
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Create Support Request</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>View Support Requests</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Pending Requests</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Resolved Requests</p>
+      </a>
+    </li>
+  </ul>
+</li>
+
+<li class="nav-item has-treeview">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-tasks"></i>
+    <p>
+      Actions
+      <i class="right fas fa-angle-left"></i>
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Add Action</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>View All Actions</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Actions by Customer</p>
+      </a>
+    </li>
+  </ul>
+</li>
+
+<li class="nav-item has-treeview">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-user"></i>
+    <p>
+      Customers
+      <i class="right fas fa-angle-left"></i>
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Add Customer</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>View Customers</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Customer Details</p>
+      </a>
+    </li>
+  </ul>
+</li>
+
+
+        <li class="nav-item has-treeview">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-users"></i>
+    <p>
+      Users
+      <i class="right fas fa-angle-left"></i>
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Add User</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>View Users</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>User Roles</p>
+      </a>
+    </li>
+  </ul>
+</li>
+
+       <li class="nav-item has-treeview">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-bell"></i>
+    <p>
+      Reminders
+      <i class="right fas fa-angle-left"></i>
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Add Reminder</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>View Reminders</p>
+      </a>
+    </li>
+  </ul>
+</li>
+
       </ul>
     </nav>
   </div>
